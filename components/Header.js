dc58f35e4dec9ca366c74b9e6ee0cc66bb18c599
ishitav <ivasisht@usc.edu>
2025-02-06 02:01:37 -0800

    make header larger

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <nav className="w-full fixed top-0 left-0 bg-white shadow-md py-3 z-50 min-h-[64px]">
+    <nav className="w-full fixed top-0 left-0 bg-white shadow-md py-3 z-50 min-h-[80px]">
       <div className="max-w-5xl mx-auto flex justify-between items-center px-8">
         
         {/* 🔹 Left - Name (Clickable Home Button) */}
