091fcde8b59069f8241d7039d5175af089a66074
ishitav <ivasisht@usc.edu>
2025-02-05 17:56:36 -0800

    fixing bottom words

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -24,7 +24,7 @@ export default function About() {
               Hello there!
             </h1>
             <p className="mt-4 text-lg text-gray-700 leading-relaxed">
-            I'm Ishita Vasishtha, a junior at the University of Southern California studying Electrical and Computer Engineering. My work focuses primarily on hardware design, machine learning and wireless systems. I've had the amazing oppurtunity to be a part of the Wireless Systems and Devices Group and the Autonomous Research Netowork Group during my time here. My research focuses primarily on machine learning and wireless systems. I'm particularly interested in hardware design, edge computing and how hardware can be used for biotech applications! Outside of engineering, I enjoy chess puzzles, jewelry making, and exploring new LA cafes. I'm always open to new opportunities! If you think I’d be a great fit for a project, research, or collaboration, feel free to reach out—I’d love to connect.
+            I'm Ishita Vasishtha, a junior at the University of Southern California studying Electrical and Computer Engineering. My work focuses primarily on hardware design, machine learning and wireless systems. I've had the amazing oppurtunity to be a part of the Wireless Systems and Devices Group and the Autonomous Research Netowork Group during my time here. My research focuses primarily on machine learning and wireless systems. I'm particularly interested in hardware design, edge computing and how hardware can be used for biotech applications! Outside of engineering, I enjoy chess puzzles, jewelry making, and exploring new LA cafes. I'm always open to new opportunities! 
             </p>
           </div>
         </div>
@@ -32,7 +32,12 @@ export default function About() {
         {/* 🔹 Quick Info Section (More Spread Out & Readable) */}
         <div className="mt-12 border-t border-gray-400 pt-6 text-lg text-gray-700">
           <div className="flex flex-col sm:flex-row justify-between sm:space-x-10">
+            <div>
+               <br />
+              If you think I’d be a great fit for a project, research, or collaboration, feel free to reach out—I’d love to connect.
+            </div>
            
+          
           </div>
         </div>
 
